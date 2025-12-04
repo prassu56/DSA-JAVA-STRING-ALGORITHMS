b606676b07760b558c6b8f0c9a7878cc553f2a2f
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2025-12-04 14:52:40 +0530

    Add files via upload

---
+++ RunLengthEncoding.java
@@ -0,0 +1,18 @@
+public class RunLengthEncoding {
+    public static void main(String[] args) {
+        String str="aaabbbbccddd";
+        String res="";
+        for(int i=0;i<str.length();i++){
+            char s=str.charAt(i);
+            if(res.contains(String.valueOf(s)))
+                continue;
+            int count=0;
+            for(int j=0;j<str.length();j++){
+                if(str.charAt(j)==s){
+                    count++;
+                }
+            }res=res+s+count;
+            }System.out.println(res);
+        
+    }
+}
